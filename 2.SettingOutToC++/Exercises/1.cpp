f0dfc0edba12c7c8a07d09f20b37d9fd5089f92b
HuangStomach <nxmbest@qq.com>
2019-09-24 11:29:20 +0800

    2.SettingOutToC++

---
+++ 2.SettingOutToC++/Exercises/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "huangstomach" << endl;
+    return 0;
+}
---
+++ 2.SettingOutToC++/Exercises/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    long l;
+    cout << "pls enter long: " << endl;
+    cin >> l;
+    l *= 220;
+    cout << "You have " << l << " ...码" << endl;
+    return 0;
+}
