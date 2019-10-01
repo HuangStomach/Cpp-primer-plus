04d5803f413d058aef3f3c110c4cc1fb55087e7e
HuangStomach <nxmbest@qq.com>
2019-10-01 10:21:48 +0800

    3.DealingWithData

---
+++ 3.DealingWithData/3.5.chartype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    char ch;
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+
+    return 0;
+}
