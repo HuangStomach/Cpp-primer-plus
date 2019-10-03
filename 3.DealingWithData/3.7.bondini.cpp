8181bdd9ff06a453e38a639fca332d6a19740c6f
HuangStomach <nxmbest@qq.com>
2019-10-03 12:05:43 +0800

    3.DealingWithData

---
+++ 3.DealingWithData/3.7.bondini.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+main() {
+    using namespace std;
+    cout << "\aOperation \"HyperHype\" is now activated!\n";
+    cout << "Enter your agent code :_______\b\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\aYou enteted " << code << "...\n";
+    cout << "\aCode verified! proceed with Plan Z3!\n";
+
+    return 0;
+}
