cdee9460979a45785ea94b9deba8cfae8a8c18ba
HuangStomach <nxmbest@qq.com>
2019-10-05 12:21:32 +0800

    3.DealingWithData

---
+++ 3.DealingWithData/3.9.fltadd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    float a = 2.34E+22f;
+    float b = a + 1.0f;
+
+    cout << "a = " << a << endl;
+    cout << "b - a = " << b - a << endl;
+
+    return 0;
+}
