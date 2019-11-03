3e77c98d1cd48afb460a8514880a8ae72bbe8522
HuangStomach <nxmbest@qq.com>
2019-11-03 20:22:08 +0800

    4.CompoundTypes

---
+++ 4.CompoundTypes/4.20.ptrstr.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    char animal[20] = "bear";
+    const char * bird = "wren";
+    char * ps;
+
+    cout << animal << " and ";
+    cout << bird << "\n";
+
+    cout << "Enter a kind of animal: ";
+    cin >> animal;
+
+    ps = animal;
+    cout << ps << "!\n";
+    cout << "Before using strcpy():\n";
+    cout << animal << " at " << (int *)animal << endl;
+    cout << ps << " at " << (int *)ps << endl;
+
+    ps = new char[strlen(animal)] + 1;
+    strcpy(ps, animal);
+    cout << "After using strcpy():\n";
+    cout << animal << " at " << (int *)animal << endl;
+    cout << ps << " at " << (int *)ps << endl;
+    delete [] ps;
+
+    return 0;
+}
