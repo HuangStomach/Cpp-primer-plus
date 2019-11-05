7f810a45d274fbdc493980bf5827c1d230de2bc0
HuangStomach <nxmbest@qq.com>
2019-11-05 16:51:42 +0800

    4.CompoundTypes

---
+++ 4.CompoundTypes/4.22.delete.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+char * getname(void);
+
+int main(int argc, char const *argv[]) {
+    char * name;
+    name = getname();
+    cout << name << " at " << (int *)name << "\n";
+    delete[] name;
+
+    name = getname();
+    cout << name << " at " << (int *)name << "\n";
+    delete[] name;
+
+    return 0;
+}
+
+char * getname() {
+    char temp[80];
+    cout << "Enter last name: ";
+    cin >> temp;
+    char * pn = new char[strlen(temp) + 1];
+    strcpy(pn, temp);
+
+    return pn;
+}
