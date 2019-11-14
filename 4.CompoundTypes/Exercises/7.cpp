a98d96ef415bad568986c48b829be3064e365608
HuangStomach <nxmbest@qq.com>
2019-11-14 18:08:44 +0800

    4.CompoundTypes

---
+++ 4.CompoundTypes/Exercises/7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pizza {
+    string ent;
+    double d;
+    double weight;
+};
+
+int main(int argc, char const *argv[]) {
+    Pizza * pizza = new Pizza;
+    cout << "请输入披萨公司名称：";
+    getline(cin, pizza->ent);
+    cout << "请输入披萨直径：";
+    cin >> pizza->d;
+    cout << "请输入披萨重量：";
+    cin >> pizza->weight;
+    
+
+    cout << "公司名称: " << pizza->ent << endl;
+    cout << "直径: " << pizza->d << endl;
+    cout << "重量: " << pizza->weight << endl;
+
+    delete pizza;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cpp-primer-plus
 The code and the answer of the C++ primer plus
+
+``` bash
+$ g++ *.cpp && ./a.out
+```
