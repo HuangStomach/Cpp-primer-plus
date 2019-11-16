d24b194b89b8c09ac2359b93dc47fd8e765f3915
HuangStomach <nxmbest@qq.com>
2019-11-16 21:47:08 +0800

    5.Loops&RelationalExpressions

---
+++ 5.Loops&RelationalExpressions/5.1.forloop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    int i;
+    for (i = 0; i < 5; i++) {
+        cout << "C++ knows loops.\n";
+    }
+    cout << "C++ knows when to stop." << endl;
+    return 0;
+}
