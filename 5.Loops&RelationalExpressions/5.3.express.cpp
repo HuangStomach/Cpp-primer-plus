6c6ce3afe6054bde7d9b846106c01b57f8c52591
HuangStomach <nxmbest@qq.com>
2019-11-18 20:05:27 +0800

    5.Loops&RelationalExpressions

---
+++ 5.Loops&RelationalExpressions/5.3.express.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    int x;
+
+    cout << "The xpression x = 100 has value ";
+    cout << (x = 100) << endl;
+    cout << "Now x = " << x << endl;
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    cout.setf(ios_base::boolalpha);
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+
+    return 0;
+}
