61ce69f7966c4aa54a69dcb9f02f68949f8f986e
HuangStomach <nxmbest@qq.com>
2019-11-19 17:46:57 +0800

    5.Loops&RelationalExpressions

---
+++ 5.Loops&RelationalExpressions/5.6.forstr1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    cout << "Enter a word: ";
+    string word;
+    cin >> word;
+
+    for (int i = word.size(); i >= 0; i--) cout << word[i];
+
+    cout << "\nBye.\n";
+    return 0;
+}
