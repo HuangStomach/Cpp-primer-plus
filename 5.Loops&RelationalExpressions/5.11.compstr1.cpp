bca34ea284e91a9fdd6ddefe596fdd5d786c1b2b
HuangStomach <nxmbest@qq.com>
2019-11-24 18:04:14 +0800

    5.Loops&RelationalExpressions

---
+++ 5.Loops&RelationalExpressions/5.11.compstr1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    char word[5] = "?ate";
+
+    for (char ch = 'a'; strcmp(word, "mate"); ch++) {
+        cout << word << endl;
+        word[0] = ch;
+    }
+    cout << "After loop ends, word is " << word << endl;
+    
+    return 0;
+}
