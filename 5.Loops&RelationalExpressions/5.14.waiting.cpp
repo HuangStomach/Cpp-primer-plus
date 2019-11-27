a1c5addb91f7dbff2c39510977ea0d26e404662e
HuangStomach <nxmbest@qq.com>
2019-11-27 17:50:20 +0800

    5.Loops&RelationalExpressions

---
+++ 5.Loops&RelationalExpressions/5.14.waiting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    cout << "Enter the delay time, in seconds: ";
+    float secs;
+    cin >> secs;
+
+    clock_t delay = secs * CLOCKS_PER_SEC;
+    cout << "starting\a\n";
+    clock_t start = clock();
+    while (clock() - start < delay) {
+        
+    }
+    
+    cout << "done\a\n";
+    return 0;
+}
