82ad74658e25eb84ea4e23778dd3e79b89374356
HuangStomach <nxmbest@qq.com>
2020-01-08 17:15:52 +0800

    7.Functions

---
+++ 7.Functions/7.6.arrfun2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+const int ArSize = 8;
+int sum_arr(int arr[], int n);
+
+int main(int argc, char const *argv[]) {
+    int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
+    std::cout << cookies << " = array address, ";
+    std::cout << sizeof(cookies) << " = size of cookies\n";
+    int sum = sum_arr(cookies, ArSize);
+    std::cout << "Total cookies eaten: " << sum << std::endl;
+    int sum = sum_arr(cookies, 3);
+    std::cout << "Total cookies eaten: " << sum << std::endl;
+    int sum = sum_arr(cookies + 4, 4);
+    std::cout << "Total cookies eaten: " << sum << std::endl;
+    return 0;
+}
+
+int sum_arr(int arr[], int n) {
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        total += arr[i];
+    }
+    return total;
+}
