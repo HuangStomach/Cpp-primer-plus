76872e03992e22f0252c74e1abedec8831752a6d
HuangStomach <nxmbest@qq.com>
2020-02-03 10:09:33 +0800

    8.AdventuresInFunctions

---
+++ 8.AdventuresInFunctions/8.2.firstref.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+    int rats = 101;
+    int & rodents = rats;
+
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+    rodents++;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    cout << "rats address = " << &rats;
+    cout << ", rodents address = " << &rodents << endl;
+
+    return 0;
+}
