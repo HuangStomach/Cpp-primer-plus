c380c4fd3ebfb4bea8125af720d6ef7f25fd956f
HuangStomach <nxmbest@qq.com>
2020-03-16 10:37:13 +0800

    10.Objects&Classes

---
+++ 10.Objects&Classes/10.5.stock10.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "10.4.stock10.h"
+
+Stock::Stock() {
+    std::cout << "Default\n";
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+
+Stock::Stock(const std::string & co, long n, double pr) {
+    std::cout << "Constructor using" << co << "\n";
+    company = co;
+    shares = n >= 0 ? n : 0;
+    share_val = pr;
+    set_tot();
+}
+
+Stock::~Stock() {
+    std::cout << "Bye." << "!\n";
+}
+
+void Stock::buy(long num, double price) {
+    if (num < 0) {
+        std::cout << "negative" << std::endl;
+    }
+    else {
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price) {
+    using std::cout;
+    using std::endl;
+    if (num < 0 || num > shares) {
+        cout << "negative" << endl;
+        return;
+    }
+    shares -= num;
+    share_val = price;
+    set_tot();
+}
+
+void Stock::update(double price) {
+    share_val = price;
+    set_tot();
+}
+
+void Stock::show() {
+    std::cout << "Company" << company
+        << " Shares: " << shares << "\n"
+        << " Share Price: $" << share_val
+        << " Total Worth: $" << total_val << "\n";
+}
