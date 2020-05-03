d1c4785a5789b65d8076c513698c2a8585739d4a
HuangStomach <nxmbest@qq.com>
2020-05-03 22:34:17 +0800

    12.Classes&DynamicMemoryAllocation

---
+++ 12.Classes&DynamicMemoryAllocation/12.4.string1.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#ifndef STRNG1_H_
+#define STRNG1_H_
+
+using std::ostream;
+using std::istream;
+
+class String {
+private:
+    char * str;
+    int len;
+    static int num_strings;
+    static const int CINLIM = 80;
+public:
+    String(const char * s);
+    String();
+    String(const String &);
+    ~String();
+    int length() const { return len; };
+
+    String & operator=(const String &);
+    String & operator=(const char *);
+    char & operator[](int i);
+    const char & operator[](int i) const;
+
+    friend bool operator<(const String &st, const String &st2);
+    friend bool operator>(const String &st1, const String &st2);
+    friend bool operator==(const String &st, const String &st2);
+
+    friend std::ostream & operator<<(std::ostream & os, const String & st);
+    friend std::istream & operator>>(std::istream & is, const String & st);
+
+    static int HowMany();
+};
+#endif
