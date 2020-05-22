b40b5434e3f6b51a320e311c56e2bcbb3d93d645
HuangStomach <nxmbest@qq.com>
2020-05-22 16:11:58 +0800

    13.ClassInheritance

--- 13.ClassInheritance/13.4.tabtenn1.h
+++ 13.ClassInheritance/13.4.tabtenn1.h
@@ -9,7 +9,7 @@ private:
     string lastname;
     bool hasTable;
 public:
-    TableTennisPlayer(const string & fn = "none", const string ln = "none", bool ht = false);
+    TableTennisPlayer(const string & fn = "none", const string & ln = "none", bool ht = false);
     void Name() const;
     bool HasTable() const { return hasTable; };
     void ResetTable(bool v) { hasTable = v; };
