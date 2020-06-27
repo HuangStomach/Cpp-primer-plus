158fd6fd76f4f8aa3695091706917a24cf2c48b2
HuangStomach <nxmbest@qq.com>
2020-06-27 16:20:10 +0800

    13.ClassInheritance

--- 13.ClassInheritance/13.15.dma.cpp
+++ 13.ClassInheritance/13.15.dma.cpp
@@ -31,3 +31,53 @@ std::ostream & operator<<(std::ostream & os, const baseDMA & rs) {
     os << "Rating: " << rs.rating << std::endl;
     return os;
 }
+
+lacksDMA::lacksDMA(const char * c, const char * l, int r): baseDMA(l, r) {
+    std::strncpy(color, c, 39);
+    color[39] = '\0';
+}
+
+lacksDMA::lacksDMA(const char * c, const baseDMA & rs): baseDMA(rs) {
+    std::strncpy(color, c, COL_LEN - 1);
+    color[COL_LEN - 1] = '\0';
+}
+
+std::ostream & operator<<(std::ostream & os, const lacksDMA & ls) {
+    os << (const baseDMA &)ls << std::endl;
+    os << "Color: " << ls.color << std::endl;
+    return os;
+}
+
+hasDMA::hasDMA(const char * s, const char * l, int r): baseDMA(l, r) {
+    style = new char[std::strlen(s) + 1];
+    std::strcpy(style, s);
+}
+
+hasDMA::hasDMA(const char * s, const baseDMA & rs): baseDMA(rs) {
+    style = new char[std::strlen(s) + 1];
+    std::strcpy(style, s);
+}
+
+hasDMA::hasDMA(const hasDMA & hs): baseDMA(hs) {
+    style = new char[std::strlen(hs.style) + 1];
+    std::strcpy(style, hs.style);
+}
+
+hasDMA::~hasDMA() {
+    delete []style;
+}
+
+hasDMA & hasDMA::operator=(const hasDMA & hs) {
+    if (this == &hs) return *this;
+    baseDMA::operator=(hs);
+    delete []style;
+    style = new char[std::strlen(hs.style) + 1];
+    std::strcpy(style, hs.style);
+    return *this;
+}
+
+std::ostream & operator<<(std::ostream & os, const hasDMA & hs) {
+    os << (const baseDMA &) hs;
+    os << "Style: " << hs.style << std::endl;
+    return os;
+}
