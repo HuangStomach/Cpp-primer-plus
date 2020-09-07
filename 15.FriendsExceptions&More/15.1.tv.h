84fbda0862194b6eb260194e9d2b651a87d19073
HuangStomach <nxmbest@qq.com>
2020-09-07 16:14:13 +0800

    15.FriendsExceptions&More

--- 15.FriendsExceptions&More/15.1.tv.h
+++ 15.FriendsExceptions&More/15.1.tv.h
@@ -35,12 +35,12 @@ public:
     Remote(int m = Tv::TV): mode(m) {}
     bool volup(Tv & t) { return t.volup(); }
     bool voldown(Tv & t) { return t.voldown(); }
-    bool onoff(Tv & t) { t.onoff(); }
-    bool chanup(Tv & t) { t.chanup(); }
-    bool chandown(Tv & t) { t.chandown(); }
-    bool set_chan(Tv & t, int c) { t.channel = c; }
-    bool set_mode(Tv & t) { t.set_mode(); }
-    bool set_input(Tv & t) { t.set_input(); }
+    void onoff(Tv & t) { t.onoff(); }
+    void chanup(Tv & t) { t.chanup(); }
+    void chandown(Tv & t) { t.chandown(); }
+    void set_chan(Tv & t, int c) { t.channel = c; }
+    void set_mode(Tv & t) { t.set_mode(); }
+    void set_input(Tv & t) { t.set_input(); }
 };
 
 #endif
