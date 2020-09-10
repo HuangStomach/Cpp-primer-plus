4581a65db7200e2b304f392c161fa3c5fbef29b3
HuangStomach <nxmbest@qq.com>
2020-09-10 16:13:54 +0800

    15.FriendsExceptions&More

---
+++ 15.FriendsExceptions&More/15.7.error1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+double hmean(double a, double b);
+
+int main(int argc, char const *argv[]) {
+    double x, y, z;
+    cout << "Enter two numbers: ";
+    while (cin >> x >> y) {
+        z = hmean(x, y);
+        cout << "Harmonic mean of " << x << " and " << y << " is " << z << endl;
+        cout << "Enter next set of numbers <q to quit>: ";
+    }
+    cout << "Bye!\n";
+    
+    return 0;
+}
+
+double hmean(double a, double b) {
+    if (a == -b) {
+        cout << "untenable\n";
+        abort();
+    }
+    return 2.0 * a * b / (a + b);
+}
