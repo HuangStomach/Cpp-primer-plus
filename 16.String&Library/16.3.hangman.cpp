693b6ee2efc015e5bbd3b019c70a170b5b0bfad4
HuangStomach <nxmbest@qq.com>
2020-10-27 15:54:01 +0800

    16.String&Library

--- 16.String&Library/16.3.hangman.cpp
+++ 16.String&Library/16.3.hangman.cpp
@@ -17,5 +17,55 @@ int main(int argc, char const *argv[]) {
     srand(time(0));
     char play;
     cout << "will you play a word game? <y/n> ";
+
+    cin >> play;
+    play = tolower(play);
+    while (play == 'y') {
+        string target = wordlist[std::rand() % NUM];
+        int length = target.length();
+        string attempt(length, '-');
+        string badchars;
+        int guesses = 6;
+        
+        while (guesses > 0 && attempt != target) {
+            char letter;
+            cout << "Guess: ";
+            cin >> letter;
+            if (badchars.find(letter) != string::npos 
+            || attempt.find(letter) != string::npos) {
+                cout << "try again.\n";
+                continue;
+            }
+            int loc = target.find(letter);
+            if (loc == string::npos) {
+                cout << "Oh, bad guess!\n";
+                --guesses;
+                badchars += letter;
+            }
+            else {
+                cout << "Good!\n";
+                attempt[loc] = letter;
+                loc = target.find(letter, loc + 1);
+                while (loc != string::npos) {
+                    attempt[loc] = letter;
+                    loc = target.find(letter, loc + 1);
+                }
+                
+            }
+            cout << "your word: " << attempt << endl;
+            if (attempt != target) {
+                if (badchars.length() > 0) cout << "Bad choices:" << endl;
+            }
+        }
+
+        if (guesses > 0) cout << "right!\n";
+        else cout << "Sorry, the word is " << target << ".\n";
+        
+        cout << "another?<y/n>";
+        cin >> play;
+        play = tolower(play);
+    }
+    
+
     return 0;
 }
