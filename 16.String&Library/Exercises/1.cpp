cd867a65e32d79b04eb95a21612c071e8f64313c
HuangStomach <nxmbest@qq.com>
2021-01-29 17:20:43 +0800

    16.String&Library

---
+++ 16.String&Library/Exercises/1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool palindrome(string);
+
+int main(int argc, char const *argv[]) {
+    cout << "Enter words (enter quit to quit):\n";
+    string input;
+    while (cin >> input && input != "quit")  {
+        cout << (palindrome(input) ? "yes" : "no") << endl;
+    }
+
+    return 0;
+}
+
+
+bool palindrome(string str) {
+    int size = str.length();
+    for (int i = 0; i < size / 2; i++) {
+        if (str[i] != str[size - i - 1]) return false;
+    }
+    return true;
+}
