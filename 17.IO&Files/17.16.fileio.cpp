da48dcee56163a9287830b442876916d12e6ca3a
HuangStomach <nxmbest@qq.com>
2021-02-09 13:35:17 +0800

    17.IO&Files

---
+++ 17.IO&Files/17.16.fileio.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char const *argv[]) {
+    using namespace std;
+
+    string filename;
+    cout << "name for new file: ";
+    cin >> filename;
+
+    ofstream fout(filename.c_str());
+
+    fout << "For your eyes only!\n";
+    cout << "Enter your secret number: ";
+    float secret;
+    cin >> secret;
+    fout << "your secret number is " << secret << endl;
+    fout.close();
+
+    ifstream fin(filename.c_str());
+    cout << "Here are the contents of " << filename << ":\n";
+    char ch;
+    while (fin.get(ch)) cout << ch;
+    cout << "Done.\n";
+    fin.close();
+
+    return 0;
+}
