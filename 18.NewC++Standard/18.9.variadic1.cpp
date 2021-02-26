eb850ccb26191d037cfaa8ed3c13cc1a98e21b1a
HuangStomach <nxmbest@qq.com>
2021-02-26 14:31:15 +0800

    18.NewC++Standard

---
+++ 18.NewC++Standard/18.9.variadic1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+void show_list3() {}
+using namespace std;
+
+template<typename T, typename... Args>
+void show_list3(T value, Args... args) {
+    cout << value << ", ";
+    show_list3(args)
+}
+
+int main(int argc, char const *argv[]) {
+    int n = 14;
+    double x = 2.71828;
+    string mr = "Mr. String objects!";
+    show_list3(n, x);
+    show_list3(x * x, '!', 7, mr);
+
+    return 0;
+}
